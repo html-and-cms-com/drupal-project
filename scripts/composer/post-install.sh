bb505e41d10df5d5e98a2086eab92a0a6792e016
Alexandr <sanya.davyskiba@gmail.com>
2015-12-15 11:07:28 +0200

    Set file mode of default local settings file.
    According to https://www.drupal.org/documentation/install/settings-file file settings.php and services.yml may have 644 mode.

--- scripts/composer/post-install.sh
+++ scripts/composer/post-install.sh
@@ -4,7 +4,7 @@
 if [ ! -f web/sites/default/settings.php ]
   then
     cp web/sites/default/default.settings.php web/sites/default/settings.php
-    chmod 777 web/sites/default/settings.php
+    chmod 644 web/sites/default/settings.php
     echo "\$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';" >> web/sites/default/settings.php
 fi
 
@@ -12,7 +12,7 @@ fi
 if [ ! -f web/sites/default/services.yml ]
   then
     cp web/sites/default/default.services.yml web/sites/default/services.yml
-    chmod 777 web/sites/default/services.yml
+    chmod 644 web/sites/default/services.yml
 fi
 
 # Prepare the files directory for installation
