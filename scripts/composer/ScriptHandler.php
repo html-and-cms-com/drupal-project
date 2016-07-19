50cc804b1d7bd92e616376e3112b8a7f656c0656
Sergey Pustynnykh <seryoga.pust@gmail.com>
2016-07-19 11:11:23 +0300

    Wrong place for new options in settings

--- scripts/composer/ScriptHandler.php
+++ scripts/composer/ScriptHandler.php
@@ -46,11 +46,11 @@ class ScriptHandler {
 
     // Prepare the settings file for installation
     if (!$fs->exists($root . '/sites/default/settings.php') and $fs->exists($root . '/sites/default/default.settings.php')) {
+      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
       // Set hash_salt
       file_put_contents($root . '/sites/default/settings.php', static::getHashSaltVariableString(), FILE_APPEND);
       // Set sync dir
       file_put_contents($root . '/sites/default/settings.php', static::getConfigSyncDirectoryVariableString(), FILE_APPEND);
-      $fs->copy($root . '/sites/default/default.settings.php', $root . '/sites/default/settings.php');
       $fs->chmod($root . '/sites/default/settings.php', 0666);
       $event->getIO()->write("Create a sites/default/settings.php file with chmod 0666");
     }
